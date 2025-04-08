2a85cf28c9b28e238a68a1a8b90c319d80003375
Kayla Mkandawire <=>
2025-04-08 14:58:11 -0500

    win condition is reachable

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+
+import io
+from contextlib import redirect_stdout
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/play_game', methods=['POST'])
+def play_game():
+    f = io.StringIO()
+    with redirect_stdout(f):
+        start_game()
+    output = f.getvalue()
+    return jsonify({'output': output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 count = 0
 

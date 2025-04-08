9c1ffe5c0e30cbcab24a94a7049884719df61102
Kayla Mkandawire <=>
2025-04-08 16:43:25 -0500

    committing changes

--- app.py
+++ app.py
@@ -4,15 +4,9 @@ from contextlib import redirect_stdout
 from main import start_game
 app = Flask(__name__)
 
-
-@app.route('/play_game', methods=['POST'])
-def play_game():
-    f = io.StringIO()
-    with redirect_stdout(f):
-        start_game()
-    output = f.getvalue()
-    return jsonify({'output': output})
-
+@app.route('/')
+def home():
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
